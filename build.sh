a79d68bee9a9e95d8187c891105f761bc9ff178c
zhubangbang-win <79228807@qq.com>
2019-09-24 14:42:21 +0800

    MODI

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-rm -rf dist && mkdir dist &&
+git submodule init
+git submodule update
+rm -rf generated && mkdir generated &&
 ./clean-cmake.sh && ./build-wasm.sh &&
 ./clean-cmake.sh
-rm -rf docs/dist && cp -r dist/ docs/dist/
 echo Done
